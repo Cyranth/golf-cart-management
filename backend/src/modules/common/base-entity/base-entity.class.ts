e14d76edde173069d24232788b6401a02c8d6e87
SDToews <33942967+SDToews@users.noreply.github.com>
2024-04-29 12:37:57 -0700

    added common modules

---
+++ backend/src/modules/common/base-entity/base-entity.class.ts
@@ -0,0 +1,18 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id!: number;
+
+  @CreateDateColumn()
+  createdOn!: Date;
+
+  @UpdateDateColumn()
+  lastUpdated!: Date;
+}
---
+++ backend/src/modules/common/repository/repository.factory.ts
@@ -0,0 +1,9 @@
+import { DataSource, EntitySchema } from "typeorm";
+import { Repository } from "./repository.class";
+
+export type { Repository };
+
+export const createRepository = <TEntity>(
+  entity: EntitySchema<TEntity>,
+  dataSource: DataSource,
+) => new Repository<TEntity>(entity, dataSource);
